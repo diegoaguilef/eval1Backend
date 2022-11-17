7272916ec7d2c1e1312466b1baad8856645ca478
Diego <dpaguilef@resit.cl>
2022-11-17 17:21:06 -0300

    adds new sections

--- printers/urls.py
+++ printers/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('new/', views.new, name='new'),
+    path('', views.home, name='home'),
+    path('whoweare', views.whoweare, name='whoweare'),
     path('<str:printer_id>/edit/', views.edit, name='edit'),
     path('create', views.create, name='create'),
     path('<int:printer_id>/', views.show, name='show'),
